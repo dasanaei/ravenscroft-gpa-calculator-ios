b150032212d6188e15aa9a7dadcffb074f05d42b
Dante Sanaei <Dsanaei@cpe-2606-a000-1124-c0b6-88c1-48aa-7e75-a637.dyn6.twc.com>
2017-02-22 22:15:55 -0500

    Initial Commit

---
+++ Ravenscroft GPA Calculator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Ravenscroft GPA Calculator
+//
+//  Created by Dante Sanaei on 2/22/17.
+//  Copyright © 2017 Competitive Coding Club. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
